308b32fa3d076d4e490440a46d16d43524981609
amitagarwal23122002 <72292057+amitagarwal23122002@users.noreply.github.com>
2020-10-03 11:27:53 +0530

    Create master.py

---
+++ master.py
@@ -0,0 +1,14 @@
+#we can run camera through our webcam using python
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+
+while True:
+  #Capture the video frame by frame
+  frame = video.read()
+  
+  cv2.imshow('frame', frame)
+
+video.release
+cv2.destroyAllWindows() 
